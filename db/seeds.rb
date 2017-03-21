6b586ee34f35e736d39a108eeb8f4589c3e81357
tjr05d <tjr05d@my.fsu.edu>
2017-03-21 09:46:53 -0400

    9:45 update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CodeSchool.destroy_all
+User.destroy_all
+Project.destroy_all
+
+wyncode = CodeSchool.create(name: "Wyncode")
+gen_assembly = CodeSchool.create(name: "Gen. Assembly")
+
+juha = User.create(name: "Juha", email: "juha@wyncode.co", age: 35, code_school: wyncode)
+tim = User.create(name: "Tim", email: "tim@wyncode.co", age: 30, code_school: wyncode)
+jo = User.create(name: "Jo", email: "jo@wyncode.co", age: 34, code_school: wyncode)
+ed = User.create(name: "Ed", email: "ed@wyncode.co", age: 24, code_school: wyncode)
+
+
+Project.create(user: tim, source: "www.github.com/stuff", url:"www.google.com")
